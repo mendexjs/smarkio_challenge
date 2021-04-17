946e2b8bec6bbfd8f1665acb3346a36ab8f47510
Matheus Mendes <matheus.iba2012@gmail.com>
2021-04-17 17:56:07 -0300

    first successful text to speech test

--- watson_tests/text_to_speech.js
+++ watson_tests/text_to_speech.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const TextToSpeechV1 = require('ibm-watson/text-to-speech/v1');
 const { IamAuthenticator } = require('ibm-watson/auth');
 const env = require('../public/assets/environment');
@@ -8,3 +9,22 @@ const textToSpeech = new TextToSpeechV1({
   }),
   serviceUrl: env.environment.ibm.urls.text_to_speech,
 });
+
+const myText = {
+  text: 'Do. Or do not. There is no try.',
+  accept: 'audio/wav',
+  voice: 'en-US_AllisonV3Voice',
+};
+
+textToSpeech.synthesize(myText)
+  .then(response => {
+    // only necessary for wav formats,
+    // otherwise `response.result` can be directly piped to a file
+    return textToSpeech.repairWavHeaderStream(response.result);
+  })
+  .then(buffer => {
+    fs.writeFileSync('watson_tests/wise_words.wav', buffer);
+  })
+  .catch(err => {
+    console.log('algo deu errado:', err);
+  });
